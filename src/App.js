0a254a056efc72bcb0427950cb68d9d5a273ae2d
BrandoSoft <csbrandox@gmail.com>
2018-07-08 17:53:51 +0200

    added searchbox and make filtering depends on whats write on searchbox

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import CardList from './CardList';
+import { cats } from './cats';
+import SearchBox from './SearchBox';
+
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			cats: cats,
+			searchField: '',
+		}
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchField: event.target.value });
+	}
+
+	render() {
+		const filteredCats = this.state.cats.filter(cats => {
+			return cats.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+		}) 
+
+		return(
+			<div className='tc'>
+				<h1> Cat Friends </h1>
+				<SearchBox searchChange={this.onSearchChange} />
+				<CardList cats={filteredCats} />
+			</div>
+);
+	}
+}
+
+export default App;
